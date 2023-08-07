dc75a11821803cf57bb0828dc00c8960c1fd70fb
Emma <im.emm@live.com>
2023-08-07 18:35:35 -0300

    Installing Flowbite

--- front/tailwind.config.js
+++ front/tailwind.config.js
@@ -2,6 +2,7 @@
 module.exports = {
   content: [
     "./src/**/*.{html,ts}",
+    "./node_modules/flowbite/**/*.js" 
   ],
     theme: {
     extend: {
@@ -14,5 +15,7 @@ module.exports = {
 
     },
   },
-  plugins: [],
+  plugins: [
+    require('flowbite/plugin')
+  ],
 }
