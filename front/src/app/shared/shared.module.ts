9a1f6597a5fa9393ac2c82cf8c8d844d34b838f0
Emma <im.emm@live.com>
2023-08-09 18:49:05 -0300

    #005 New Courses component

--- front/src/app/shared/shared.module.ts
+++ front/src/app/shared/shared.module.ts
@@ -3,15 +3,16 @@ import { CommonModule } from '@angular/common';
 import { HeaderComponent } from './components/header/header.component';
 import { FooterComponent } from './components/footer/footer.component';
 import { CourseCardComponent } from './components/course-card/course-card.component';
+import { NewCoursesComponent } from './components/new-courses/new-courses.component';
 
 
 @NgModule({
-  declarations: [HeaderComponent, FooterComponent, CourseCardComponent],
+  declarations: [HeaderComponent, FooterComponent, CourseCardComponent, NewCoursesComponent],
   imports: [
     CommonModule
   ],
   exports: [
-    HeaderComponent, FooterComponent, CourseCardComponent
+    HeaderComponent, FooterComponent, CourseCardComponent, NewCoursesComponent
   ],
 })
 export class SharedModule { }
