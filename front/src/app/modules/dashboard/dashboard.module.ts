9184af9f509528e6bf083e738e14c4d132e83c63
Emma <im.emm@live.com>
2023-08-09 17:58:04 -0300

    #004 Course Card component

--- front/src/app/modules/dashboard/dashboard.module.ts
+++ front/src/app/modules/dashboard/dashboard.module.ts
@@ -2,12 +2,15 @@ import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { DashboardRoutingModule } from './dashboard-routing.module';
 import { DashboardPageComponent } from './dashboard-page/dashboard-page.component';
+import { SharedModule } from 'src/app/shared/shared.module';
 
 @NgModule({
   declarations: [DashboardPageComponent],
   imports: [
     CommonModule,
     DashboardRoutingModule,
+    SharedModule,
+    
   ],
   exports: [
     DashboardPageComponent
--- front/src/app/shared/shared.module.ts
+++ front/src/app/shared/shared.module.ts
@@ -2,15 +2,16 @@ import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { HeaderComponent } from './components/header/header.component';
 import { FooterComponent } from './components/footer/footer.component';
+import { CourseCardComponent } from './components/course-card/course-card.component';
 
 
 @NgModule({
-  declarations: [HeaderComponent, FooterComponent],
+  declarations: [HeaderComponent, FooterComponent, CourseCardComponent],
   imports: [
-    CommonModule, HeaderComponent, FooterComponent
+    CommonModule
   ],
   exports: [
-    HeaderComponent, FooterComponent
+    HeaderComponent, FooterComponent, CourseCardComponent
   ],
 })
 export class SharedModule { }
