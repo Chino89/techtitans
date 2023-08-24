bdbea50fa4516d0d3ae7ceb17beca510bf3075cf
Emma <im.emm@live.com>
2023-08-24 14:33:45 -0300

    new interfaces

---
+++ src/app/core/interfaces/interfaces.ts
@@ -0,0 +1,13 @@
+type Course = {
+  id: number;
+  title: string;
+  content: string;
+  image: string;
+};
+
+type CarouselItem = {
+    img: string;
+    alt: string;
+  };
+
+export { Course, CarouselItem };
