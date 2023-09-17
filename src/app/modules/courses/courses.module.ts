5b2c6858048317c65dddba402118902620f7cf50
Emma <im.emm@live.com>
2023-09-17 13:34:29 -0300

    Admin create-course path

--- src/app/modules/courses/courses.module.ts
+++ src/app/modules/courses/courses.module.ts
@@ -2,9 +2,10 @@ import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { CoursesPageComponent } from './courses-page/courses-page.component';
 import { CoursesRoutingModule } from './courses-routing.module';
+import { NewCourseComponent } from './new-course/new-course.component';
 
 @NgModule({
-  declarations: [CoursesPageComponent],
+  declarations: [CoursesPageComponent, NewCourseComponent],
   imports: [CommonModule, CoursesRoutingModule],
 })
 export class CoursesModule {}
