66abf4abcfe5a7edb3222b17b1badf0337b01c6e
i76 <guti_545@hotmail.com>
2023-10-02 18:33:43 -0300

    02/10/23 modificacion de cursos y eliminacion de filtro

--- src/app/modules/courses/courses-page/courses-page.component.ts
+++ src/app/modules/courses/courses-page/courses-page.component.ts
@@ -1,35 +1,15 @@
 import { Component, OnInit } from '@angular/core';
-import { HttpClient } from '@angular/common/http';
-import { Course } from 'src/app/core/interfaces/interfaces';
 
 @Component({
   selector: 'app-courses-page',
   templateUrl: './courses-page.component.html',
-  styleUrls: ['./courses-page.component.css'],
+  styleUrls: ['./courses-page.component.css']
 })
 export class CoursesPageComponent implements OnInit {
-  courses: Course[] = [];
-  filteredCoursesInSection: Course[] = [];
-  selectedCategory: string = 'todos';
 
-  constructor(private http: HttpClient) {}
+  constructor() { }
 
   ngOnInit(): void {
-    this.http
-      .get<Course[]>('https://jsonplaceholder.typicode.com/posts') // Cambiar a la URL correcta de los cursos
-      .subscribe((allCourses) => {
-        this.courses = allCourses;
-        this.filteredCoursesInSection = allCourses;
-      });
   }
 
-  filterCoursesByCategoryInSection(): void {
-    if (this.selectedCategory === 'todos') {
-      this.filteredCoursesInSection = this.courses;
-    } else {
-      this.filteredCoursesInSection = this.courses.filter(
-        (curso) => curso.category === this.selectedCategory
-      );
-    }
-  }
 }
