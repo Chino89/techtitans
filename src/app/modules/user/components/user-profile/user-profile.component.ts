9ee7d158b387060cdb8b333c48050c24d03fe9a9
Emma <im.emm@live.com>
2023-10-02 01:19:05 -0300

    new components for edit, get user profile and get user courses

---
+++ src/app/modules/user/components/user-profile/user-profile.component.ts
@@ -0,0 +1,49 @@
+import { Component, OnInit } from '@angular/core';
+import { User, UserResponse } from 'src/app/core/interfaces/userInterfaces';
+import { LoginService } from 'src/app/core/services/auth/login.service';
+import { UserService } from 'src/app/core/services/users/user.service';
+
+@Component({
+  selector: 'app-user-profile',
+  templateUrl: './user-profile.component.html',
+  styleUrls: ['./user-profile.component.css'],
+})
+export class UserProfileComponent implements OnInit {
+  user: User = {
+    id: 0,
+    nombre: '',
+    apellido: '',
+    email: '',
+    roles: [''],
+    accessToken: '',
+  };
+  userDetail: UserResponse = {
+    id: 0,
+    nombre: '',
+    apellido: '',
+    email: '',
+    avatar: '',
+    public_id: null,
+    status: false,
+  };
+
+  constructor(
+    private loginService: LoginService,
+    private userService: UserService
+  ) {}
+
+  ngOnInit(): void {
+    this.loginService.currentUserData.subscribe((data) => {
+      this.user = data;
+      this.getUserDetail(this.user.id);
+    });
+  }
+
+  getUserDetail(userId: number) {
+    this.userService.getUser(userId).subscribe({
+      next: (response) => {
+        this.userDetail = response.data;
+      },
+    });
+  }
+}
--- src/app/modules/user/user.module.ts
+++ src/app/modules/user/user.module.ts
@@ -2,16 +2,22 @@ import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 
 import { UserRoutingModule } from './user-routing.module';
-import { UserPageComponent } from './user-page/user-page.component';
-
+import { UserPageComponent } from './components/user-page/user-page.component';
+import { UserDropdownComponent } from './components/user-dropdown/user-dropdown.component';
+import { UserCoursesComponent } from './components/user-courses/user-courses.component';
+import { UserEditComponent } from './components/user-edit/user-edit.component';
+import { UserProfileComponent } from './components/user-profile/user-profile.component';
 
 @NgModule({
   declarations: [
-    UserPageComponent
+    UserPageComponent,
+    UserDropdownComponent,
+    UserCoursesComponent,
+    UserEditComponent,
+    UserProfileComponent,
   ],
-  imports: [
-    CommonModule,
-    UserRoutingModule
-  ]
+
+  imports: [CommonModule, UserRoutingModule],
+  exports: [UserDropdownComponent],
 })
-export class UserModule { }
+export class UserModule {}
