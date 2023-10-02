3dba0a960b9fe2cb9db29b8edc7107ff2dc2596d
Emma <im.emm@live.com>
2023-10-02 01:15:03 -0300

    userService api call for update + delete  + get user

--- src/app/core/services/users/user.service.ts
+++ src/app/core/services/users/user.service.ts
@@ -2,8 +2,10 @@ import { Injectable } from '@angular/core';
 import { HttpClient } from '@angular/common/http';
 import { Observable } from 'rxjs';
 import {
+  GetUser,
   TeacherDataResponse,
   UserDataResponse,
+  UserEditRequest,
 } from '../../interfaces/userInterfaces';
 
 import { environment } from 'src/environments/environment';
@@ -21,6 +23,10 @@ export class UserService {
     );
   }
 
+  getUser(id: Number): Observable<GetUser> {
+    return this.http.get<GetUser>(`${environment.API_URL}/api/usuario/${id}`);
+  }
+
   getTeachers(): Observable<TeacherDataResponse> {
     return this.http.get<TeacherDataResponse>(
       `${environment.API_URL}/api/usuarios/docentes`
@@ -31,6 +37,19 @@ export class UserService {
     return this.http.post<BackEndResponse>(
       `${environment.API_URL}/api/usuarios/roles`,
       { userID, roles }
+    );
+  }
+
+  editUser(id: Number, formData: UserEditRequest) {
+    return this.http.put<BackEndResponse>(
+      `${environment.API_URL}/api/usuario/${id}/roles`,
+      { formData }
+    );
+  }
+
+  deleteUser(id: number): Observable<BackEndResponse> {
+    return this.http.delete<BackEndResponse>(
+      `${environment.API_URL}/api/usuario/${id}/borrar`
     );
   }
 }
