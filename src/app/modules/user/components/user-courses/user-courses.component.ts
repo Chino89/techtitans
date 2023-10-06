d9ad5ee4fff686ea3500e3a6aa42ae1afd133d0d
Emma <im.emm@live.com>
2023-10-06 14:25:43 -0300

    get and render TeachersCourses on userProfile

--- src/app/modules/user/components/user-courses/user-courses.component.ts
+++ src/app/modules/user/components/user-courses/user-courses.component.ts
@@ -1,11 +1,17 @@
 import { Component, OnDestroy, OnInit } from '@angular/core';
 import { Subscription } from 'rxjs';
 import {
+  CourseData,
+  CourseResponse,
+} from 'src/app/core/interfaces/courseInterfaces';
+import {
   UserEnrollment,
   UserEnrollmentData,
 } from 'src/app/core/interfaces/enrollmentInterfaces';
+import { BackEndError } from 'src/app/core/interfaces/interfaces';
 import { User, UserResponse } from 'src/app/core/interfaces/userInterfaces';
 import { LoginService } from 'src/app/core/services/auth/login.service';
+import { CourseService } from 'src/app/core/services/course/course.service';
 import { EnrollmentService } from 'src/app/core/services/enrollment/enrollment.service';
 import { UserService } from 'src/app/core/services/users/user.service';
 
@@ -34,11 +40,15 @@ export class UserCoursesComponent implements OnInit, OnDestroy {
   };
   subscriptions: Subscription[] = [];
   userCoursesResponse: UserEnrollmentData[] = [];
+  teacherCoursesResponse: CourseResponse[] = [];
+  backendErrors: BackEndError[] = [];
+  userIsLoged = false;
 
   constructor(
     private loginService: LoginService,
     private userService: UserService,
-    private enrollmentService: EnrollmentService
+    private enrollmentService: EnrollmentService,
+    private courseService: CourseService
   ) {}
   ngOnInit(): void {
     const currentUserDataServiceSubscription =
@@ -48,6 +58,31 @@ export class UserCoursesComponent implements OnInit, OnDestroy {
         this.getUserCourses();
       });
     this.subscriptions.push(currentUserDataServiceSubscription);
+
+    const currentUserLoginOnServiceSubscription =
+      this.loginService.currentUserLoginOn.subscribe({
+        next: (userIsLoged) => {
+          this.userIsLoged = userIsLoged;
+        },
+      });
+    this.subscriptions.push(currentUserLoginOnServiceSubscription);
+
+    const getAllTeacherCoursesServiceSubscription = this.courseService
+      .getAllTeacherCourses()
+      .subscribe({
+        next: (response: CourseData) => {
+          (this.teacherCoursesResponse = response.data),
+            console.log(this.teacherCoursesResponse);
+        },
+        error: (errorData) => {
+          if (errorData.error.mensaje) {
+            this.backendErrors = [{ mensaje: errorData.error.mensaje }];
+          } else {
+            this.backendErrors = errorData.error.errors as BackEndError[];
+          }
+        },
+      });
+    this.subscriptions.push(getAllTeacherCoursesServiceSubscription);
   }
 
   getUserDetail() {
