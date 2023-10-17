81f02337f96d3d8505c11aae6ff5ede00073725a
Emma <im.emm@live.com>
2023-10-17 18:58:42 -0300

    prettier

--- src/app/core/interfaces/attendanceInterface.ts
+++ src/app/core/interfaces/attendanceInterface.ts
@@ -3,47 +3,47 @@ export interface attendanceData {
 }
 
 export interface attendanceResponse {
-  id:                number;
+  id: number;
   codigoInscripcion: string;
-  asistio:           boolean;
-  puntaje:           string;
-  curso:             Curso;
-  estudiante:        Docente;
-  docente:           Docente;
-  pago:              Pago;
+  asistio: boolean;
+  puntaje: string;
+  curso: Curso;
+  estudiante: Docente;
+  docente: Docente;
+  pago: Pago;
 }
 
 export interface Curso {
-  id:          number;
-  nombre:      string;
+  id: number;
+  nombre: string;
   descripcion: string;
-  portada:     string;
-  dia_curso:   Date | null;
-  hora_curso:  string;
-  duracion:    string;
-  precio:      string;
-  lugar:       string;
-  slug:        string;
-  categoria:   Categoria;
-  usuario:     Docente;
-  docente:     Docente;
+  portada: string;
+  dia_curso: Date | null;
+  hora_curso: string;
+  duracion: string;
+  precio: string;
+  lugar: string;
+  slug: string;
+  categoria: Categoria;
+  usuario: Docente;
+  docente: Docente;
 }
 
 export interface Categoria {
-  id:     number;
+  id: number;
   nombre: string;
 }
 
 export interface Docente {
-  id:       number;
-  nombre:   string;
+  id: number;
+  nombre: string;
   apellido: string;
-  email:    string;
+  email: string;
 }
 
 export interface Pago {
-  id:        number;
+  id: number;
   tokenPago: null;
   fechaPago: Date | null;
-  pago:      boolean;
+  pago: boolean;
 }
